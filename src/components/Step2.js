e46e576d330d642f493b5606190b947fcec27f5b
Mehul Jain <mehul.jain2206@gmail.com>
2026-05-14 10:49:46 +0530

    form3

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -12,9 +12,9 @@ let one={
  let {first,last}=one;
 
     return (
-        <div id="step2">
+        <div>
             <h1>Car Details</h1>
-            <form onSubmit={((e)=>{ e.preventDefault();  if(!first.current.value || !last.current.value) return; setStep(3)})}>
+            <form  id= "step2" onSubmit={((e)=>{ e.preventDefault();  if(!first.current.value || !last.current.value) return; setStep(3)})}>
                 <label>Brand:</label>
                 <br/>
                 <input type="text" id="brand" ref={first}/>
